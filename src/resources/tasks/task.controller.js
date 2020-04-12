abd4a627a6346daca43427e22b8f8ed55575f10f
Stas Neteruk <cognes.com@gmail.com>
2020-04-12 22:14:39 +0300

    Edited task.controller

--- src/resources/tasks/task.controller.js
+++ src/resources/tasks/task.controller.js
@@ -1,20 +1,17 @@
 const HTTP_STATUS = require('http-status');
 const taskService = require('./task.service');
 const boardService = require('./../boards/board.service');
-const { sendJsonError } = require('./../../utils/response/response.utils');
+const checkUUID = require('./../../utils/checkUUID/checkUUID.utils');
+const ResourceNotFoundError = require('./../../errors/ResourceNotFoundError');
+const ValidationError = require('./../../errors/ValidationError');
 
 const getTasksTreatment = async (req, res, next) => {
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        {
-          message: `Board don't exist by id: ${boardId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     const tasks = await taskService.getAll(boardId);
     res.status(HTTP_STATUS.OK).send(tasks);
@@ -26,26 +23,16 @@ const getTasksTreatment = async (req, res, next) => {
 const getTaskTreatment = async (req, res, next) => {
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const taskId = req.params.taskId;
+    checkUUID(taskId);
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        {
-          message: `Board don't exist by id: ${boardId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     const task = await taskService.getById(boardId, taskId);
     if (!task) {
-      return sendJsonError(
-        res,
-        {
-          message: `Task don't exist by id: ${taskId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Task don't exist by id: ${taskId}`);
     }
     res.status(HTTP_STATUS.OK).send(task);
   } catch (err) {
@@ -54,26 +41,26 @@ const getTaskTreatment = async (req, res, next) => {
 };
 
 const createTaskTreatment = async (req, res, next) => {
-  const keys = Object.keys(req.body);
-  const requiredFields = ['title', 'order', 'description', 'userId', 'boardId'];
-  const isValidOperation = requiredFields.every(key => keys.includes(key));
-  if (!isValidOperation) {
-    return sendJsonError(res, {
-      message:
-        'Wrong operation! Required field: title, order, description, userId, boardId, columnId.'
-    });
-  }
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
+    const keys = Object.keys(req.body);
+    const requiredFields = [
+      'title',
+      'order',
+      'description',
+      'userId',
+      'boardId'
+    ];
+    const isValidOperation = requiredFields.every(key => keys.includes(key));
+    if (!isValidOperation) {
+      throw new ValidationError(
+        'Wrong operation! Required field: title, order, description, userId, boardId, columnId.'
+      );
+    }
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        {
-          message: `Board don't exist by id: ${boardId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     const data = req.body;
     data.boardId = boardId;
@@ -85,43 +72,34 @@ const createTaskTreatment = async (req, res, next) => {
 };
 
 const updateTaskTreatment = async (req, res, next) => {
-  const keys = Object.keys(req.body);
-  const allowedUpdates = [
-    'id',
-    'title',
-    'order',
-    'description',
-    'userId',
-    'boardId',
-    'columnId'
-  ];
-  const isValidOperation = keys.every(key => allowedUpdates.includes(key));
-  if (!isValidOperation) {
-    return sendJsonError(res, {
-      message:
-        'Wrong operation! Allowed update fields: title, order, description, userId, boardId, columnId'
-    });
-  }
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const taskId = req.params.taskId;
+    checkUUID(taskId);
+    const keys = Object.keys(req.body);
+    const allowedUpdates = [
+      'id',
+      'title',
+      'order',
+      'description',
+      'userId',
+      'boardId',
+      'columnId'
+    ];
+    const isValidOperation = keys.every(key => allowedUpdates.includes(key));
+    if (!isValidOperation) {
+      throw new ValidationError(
+        'Wrong operation! Allowed update fields: title, order, description, userId, boardId, columnId'
+      );
+    }
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        {
-          message: `Board don't exist by id: ${boardId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     const task = await taskService.update(boardId, taskId, req.body);
     if (!task) {
-      return sendJsonError(
-        res,
-        { message: `Task don't exist by id: ${taskId}` },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Task don't exist by id: ${taskId}`);
     }
     res.status(HTTP_STATUS.OK).send(task);
   } catch (err) {
@@ -132,24 +110,16 @@ const updateTaskTreatment = async (req, res, next) => {
 const deleteTaskTreatment = async (req, res, next) => {
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const taskId = req.params.taskId;
+    checkUUID(taskId);
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        {
-          message: `Board don't exist by id: ${boardId}`
-        },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     const task = await taskService.delete(boardId, taskId);
     if (!task) {
-      return sendJsonError(
-        res,
-        { message: `Task don't exist by id: ${taskId}` },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Task don't exist by id: ${taskId}`);
     }
     res.status(HTTP_STATUS.OK).send(task);
   } catch (err) {
