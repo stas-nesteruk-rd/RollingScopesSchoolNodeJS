2a6759939d35538b6d9335a76a7ecc863d087499
Stas Neteruk <cognes.com@gmail.com>
2020-04-12 22:26:25 +0300

    Edited board.controller

--- src/resources/boards/board.controller.js
+++ src/resources/boards/board.controller.js
@@ -1,6 +1,8 @@
 const HTTP_STATUS = require('http-status');
 const boardService = require('./board.service');
-const { sendJsonError } = require('./../../utils/response/response.utils');
+const checkUUID = require('./../../utils/checkUUID/checkUUID.utils');
+const ValidationError = require('./../../errors/ValidationError');
+const ResourceNotFoundError = require('./../../errors/ResourceNotFoundError');
 
 const getBoardsTreatment = async (req, res, next) => {
   try {
@@ -14,13 +16,10 @@ const getBoardsTreatment = async (req, res, next) => {
 const getBoardTreatment = async (req, res, next) => {
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const board = await boardService.getById(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        { message: `Board don't exist by id: ${boardId}` },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     res.status(HTTP_STATUS.OK).send(board);
   } catch (err) {
@@ -29,27 +28,25 @@ const getBoardTreatment = async (req, res, next) => {
 };
 
 const createBoardTreatment = async (req, res, next) => {
-  const boardKeys = Object.keys(req.body);
-  const requiredBoardFiels = ['title', 'columns'];
-  const requiredColumnFiels = ['title', 'order'];
-  let isValidOperation = requiredBoardFiels.every(key =>
-    boardKeys.includes(key)
-  );
-  if (!isValidOperation) {
-    return sendJsonError(res, {
-      message: 'Wrong operation! Send: title, columns'
-    });
-  }
-  const columnKeys = req.body.columns.map(column => Object.keys(column));
-  isValidOperation = columnKeys.map(keys =>
-    requiredColumnFiels.every(key => keys.includes(key))
-  );
-  if (isValidOperation.some(value => value === false)) {
-    return sendJsonError(res, {
-      message: 'Wrong operation! Columns should includ only: title, order'
-    });
-  }
   try {
+    const boardKeys = Object.keys(req.body);
+    const requiredBoardFiels = ['title', 'columns'];
+    const requiredColumnFiels = ['title', 'order'];
+    let isValidOperation = requiredBoardFiels.every(key =>
+      boardKeys.includes(key)
+    );
+    if (!isValidOperation) {
+      throw new ValidationError('Wrong operation! Send: title, columns');
+    }
+    const columnKeys = req.body.columns.map(column => Object.keys(column));
+    isValidOperation = columnKeys.map(keys =>
+      requiredColumnFiels.every(key => keys.includes(key))
+    );
+    if (isValidOperation.some(value => value === false)) {
+      throw new ValidationError(
+        'Wrong operation! Columns should includ only: title, order'
+      );
+    }
     const board = await boardService.create(req.body);
     res.status(HTTP_STATUS.OK).send(board);
   } catch (err) {
@@ -58,35 +55,32 @@ const createBoardTreatment = async (req, res, next) => {
 };
 
 const updateBoardTreatment = async (req, res, next) => {
-  const boardId = req.params.boardId;
-  const keys = Object.keys(req.body);
-  const allowedBoardUpdates = ['id', 'title', 'columns'];
-  const allowedColumnUpdates = ['id', 'title', 'order'];
-  let isValidOperation = keys.every(key => allowedBoardUpdates.includes(key));
-  if (!isValidOperation) {
-    return sendJsonError(res, {
-      message: 'Wrong operation! Allowed update fields: tittle, columns'
-    });
-  }
-  if (keys.some(key => key === 'columns')) {
-    const columnKeys = req.body.columns.map(column => Object.keys(column));
-    isValidOperation = columnKeys.map(ckeys =>
-      ckeys.every(key => allowedColumnUpdates.includes(key))
-    );
-    if (isValidOperation.some(value => value === false)) {
-      return sendJsonError(res, {
-        message: 'Wrong operation! Columns should includ only: title, order'
-      });
-    }
-  }
   try {
+    const boardId = req.params.boardId;
+    checkUUID(boardId);
+    const keys = Object.keys(req.body);
+    const allowedBoardUpdates = ['id', 'title', 'columns'];
+    const allowedColumnUpdates = ['id', 'title', 'order'];
+    let isValidOperation = keys.every(key => allowedBoardUpdates.includes(key));
+    if (!isValidOperation) {
+      throw new ValidationError(
+        'Wrong operation! Allowed update fields: tittle, columns'
+      );
+    }
+    if (keys.some(key => key === 'columns')) {
+      const columnKeys = req.body.columns.map(column => Object.keys(column));
+      isValidOperation = columnKeys.map(ckeys =>
+        ckeys.every(key => allowedColumnUpdates.includes(key))
+      );
+      if (isValidOperation.some(value => value === false)) {
+        throw new ValidationError(
+          'Wrong operation! Columns should includ only: title, order'
+        );
+      }
+    }
     const board = await boardService.update(boardId, req.body);
     if (!board) {
-      return sendJsonError(
-        res,
-        { message: `Board don't exist by id: ${boardId}` },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     res.status(HTTP_STATUS.OK).send(board);
   } catch (err) {
@@ -97,13 +91,10 @@ const updateBoardTreatment = async (req, res, next) => {
 const deleteBoardTreatment = async (req, res, next) => {
   try {
     const boardId = req.params.boardId;
+    checkUUID(boardId);
     const board = await boardService.delete(boardId);
     if (!board) {
-      return sendJsonError(
-        res,
-        { message: `Board don't exist by id: ${boardId}` },
-        HTTP_STATUS.NOT_FOUND
-      );
+      throw new ResourceNotFoundError(`Board don't exist by id: ${boardId}`);
     }
     res.status(HTTP_STATUS.OK).send(board);
   } catch (err) {
