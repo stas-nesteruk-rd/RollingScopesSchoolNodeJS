707e40c27c43976d07b4ba11659ed45dbef4db52
Stas Neteruk <cognes.com@gmail.com>
2020-04-15 21:31:02 +0300

    Edited loadData

--- src/db/loadData/loadData.js
+++ src/db/loadData/loadData.js
@@ -39,12 +39,12 @@ const boards = [
     title: 'Board1',
     columns: [
       {
-        id: 'e9e233b2-d7fc-44ca-a0bd-84814f92f53a',
+        _id: 'e9e233b2-d7fc-44ca-a0bd-84814f92f53a',
         title: 'column1:title',
         order: 0
       },
       {
-        id: 'a8afaef0-545f-4b0f-ad29-8c57bd4c8241',
+        _id: 'a8afaef0-545f-4b0f-ad29-8c57bd4c8241',
         title: 'column1:title',
         order: 0
       }
@@ -55,12 +55,12 @@ const boards = [
     title: 'Board2',
     columns: [
       {
-        id: 'c6591a9a-b3de-4907-acaa-ed5163d5b3f0',
+        _id: 'c6591a9a-b3de-4907-acaa-ed5163d5b3f0',
         title: 'column1:title',
         order: 0
       },
       {
-        id: 'de5f5f6c-9f57-4707-b570-53167388407b',
+        _id: 'de5f5f6c-9f57-4707-b570-53167388407b',
         title: 'column1:title',
         order: 0
       }
@@ -71,12 +71,12 @@ const boards = [
     title: 'Board3',
     columns: [
       {
-        id: '50dc0359-49e7-41af-a23d-e3faca7ce0ab',
+        _id: '50dc0359-49e7-41af-a23d-e3faca7ce0ab',
         title: 'column1:title',
         order: 0
       },
       {
-        id: '2f32fd35-969c-4336-9cd5-00122e554bd3',
+        _id: '2f32fd35-969c-4336-9cd5-00122e554bd3',
         title: 'column1:title',
         order: 0
       }
